99ba46d4bcee8d81aca56e751c178a0d2f86dcde
kaleemmd9866 <58474918+kaleemmd9866@users.noreply.github.com>
2019-12-03 20:09:09 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class P3
+{
+  public static void main(String args[])
+    {
+      int a,b;
+          
+          b=Integer.parseInt(args[0]);
+          a=Integer.parseInt(args[1]);
+System.out.println("The sum of "+a+" and "+b+" is "+(a+b)); 
+     }
+}
